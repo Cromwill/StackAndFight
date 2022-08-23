ade505bb1a34a89574912d3759ae0b6d5979e621
humbleGuy1 <vrudnevskiy@list.ru>
2022-08-23 17:53:45 +0300

    Visual

--- Assets/Scripts/Path/PathCellRenderer.cs
+++ Assets/Scripts/Path/PathCellRenderer.cs
@@ -1,14 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PathCellRenderer : MonoBehaviour
 {
     [SerializeField] private MeshRenderer _meshRenderer;
     [SerializeField] private Color _activatedColor;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
 
     public void Colorize()
     {
-        _meshRenderer.material.color = _activatedColor;
+        //_meshRenderer.material.color = _activatedColor;
+        _spriteRenderer.color = _activatedColor;
     }
 }
