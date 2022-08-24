65e91f7b5da223bf720538a63dcccf0e9aa29e94
humbleGuy1 <vrudnevskiy@list.ru>
2022-08-24 16:45:57 +0300

    camera

--- Assets/Scripts/Finish/FinishTrigger.cs
+++ Assets/Scripts/Finish/FinishTrigger.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Cinemachine;
+using System.Collections;
 
 public class FinishTrigger : Interactable
 {
@@ -8,8 +9,17 @@ public class FinishTrigger : Interactable
 
     public override void Interact(Player player)
     {
+        StartCoroutine(Interacting(player));
+    }
+
+    private IEnumerator Interacting(Player player)
+    {
         player.Mover.MoveToFinish(_finalPathPoint);
         _wallsCamera.transform.SetParent(player.transform);
+        _wallsCamera.m_LookAt = player.transform;
+
+        yield return new WaitForSeconds(0.5f);
+
         _wallsCamera.Priority = 2;
         Time.timeScale = 0.5f;
     }
