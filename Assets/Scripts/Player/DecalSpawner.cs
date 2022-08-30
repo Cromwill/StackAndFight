e895e6f8c5dc754ec19d5355eb62637765c4834f
humbleGuy1 <vrudnevskiy@list.ru>
2022-08-30 18:05:17 +0300

    DecalsUpdate

--- Assets/Scripts/Player/DecalSpawner.cs
+++ Assets/Scripts/Player/DecalSpawner.cs
@@ -10,6 +10,6 @@ public class DecalSpawner : MonoBehaviour
     public void Spawn()
     {
         Vector3 targetPosition = transform.position + transform.forward;
-        EasyDecal.Project(_decalPrefab.gameObject, targetPosition, Quaternion.Euler(0, transform.rotation.y, 0));
+        EasyDecal.Project(_decalPrefab.gameObject, targetPosition, Quaternion.identity);
     }
 }
